ed0c1cbb768c09e7d7546eed015dd981dd32d1ae
Gustavo Santos <gutopc@gmail.com>
2022-01-11 07:44:16 -0300

    Add files via upload

---
+++ Exercicio.js
@@ -0,0 +1,4 @@
+var nome = prompt("Nome do remetente: ")
+var destinatario = prompt("Nome do destinatário: ")
+var mensagem = prompt("Mensagem a ser enviada: ")
+alert('O Professor ' + nome + 'deseja a ' + destinatario + ', ' + mensagem)
